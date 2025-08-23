02d17ecd2c418dc267de45c2d8e47936d1b128de
wilberdelarosa <144499480+wilberdelarosa@users.noreply.github.com>
2025-08-22 22:46:13 -0400

    Enable multi-choice selection and highlight

--- MoodProyect/Models/Choice.cs
+++ MoodProyect/Models/Choice.cs
@@ -1,7 +1,12 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace MoodProyect.Models;
 
-public class Choice
+public partial class Choice : ObservableObject
 {
     public string Text { get; set; } = string.Empty;
     public int Value { get; set; }
+
+    [ObservableProperty]
+    bool isSelected;
 }
